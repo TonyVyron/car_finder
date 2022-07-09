65edddc51e747c9d24f9e55992da2bbd70c02306
TonyVyron <antoniofuentesjara@gmail.com>
2022-07-08 19:48:43 -0500

    Agregando fuente

--- lib/main.dart
+++ lib/main.dart
@@ -7,13 +7,26 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Material App',
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Car Findr'),
+          backgroundColor: Color.fromARGB(255, 144, 12, 48),
+          title: Text(
+            'Car Finder',
+            style: TextStyle(fontFamily: 'biko', fontWeight: FontWeight.w500),
+          ),
         ),
         body: Center(
           child: Container(
-            child: Text('Hello World'),
+            margin: EdgeInsets.all(20),
+            child: Text(
+              'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop p',
+              textAlign: TextAlign.justify,
+              style: TextStyle(
+                fontFamily: 'biko',
+                fontSize: 18,
+              ),
+            ),
           ),
         ),
       ),
